ce67b7dbcab2e6253263ea73321762ab9d538b28
WJX2001 <1758807220@qq.com>
2024-11-08 18:49:24 +0800

    project init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Test1 from '@/pages/Test1';
+
+function App() {
+  return (
+    <>
+      <Test1 />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Test1/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from'./index.module.less';
+const Test1 = () => {
+  return <div className={styles['wjx']}>index</div>;
+};
+
+export default Test1;
